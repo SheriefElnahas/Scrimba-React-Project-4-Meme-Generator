91cbc86195a75b420936316a6f172e540d11330f
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-09-07 17:07:24 +0200

    meme component - finish static setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './components/Header/Header';
 import UserInput from './components/UserInput/UserInput';
+import Meme from './components/Meme/Meme';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <UserInput />
+      < Meme />
     </div>
   )
 }
---
+++ src/components/Meme/Meme.jsx
@@ -0,0 +1,10 @@
+import './Meme.css';
+
+function Meme() {
+    return <div className="meme">
+            <img src="https://www.wearethemighty.com/uploads/legacy/assets.rbl.ms/22884259/origin.png" alt="meme image" />
+         </div>
+}
+
+
+export default Meme;
