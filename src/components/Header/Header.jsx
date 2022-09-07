0c33ab8d7ad54f10e7cb9cf200144485eb9d4c06
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-09-07 16:52:27 +0200

    header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="header-logo">
+            <h1 className='header-heading'>Meme Generator</h1>
+            </div>
+            <p className="header-text">React Course - Project 3</p>
+            
+        </header>
+    )
+}
+
+
+export default Header;
